9be236859c564c9275c6d9006933eda23bad8f7e
Daniel Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2023-02-23 01:15:36 -0400

    Update rectangle.js

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const Rectangle = ({
   x,
@@ -8,7 +9,8 @@ const Rectangle = ({
   onNextClick,
   onReturnClick,
   onCheckboxChange,
-  isChecked
+  isChecked,
+  currentWord
 }) => {
   const rectangleStyle = {
     position: "absolute",
@@ -23,7 +25,21 @@ const Rectangle = ({
   };
 
   const [showDescription, setShowDescription] = useState(false);
-  //codigo
+
+  //the description button
+  const CircleButton = styled.button`
+    width: 25px;
+    height: 20px;
+    border-radius: 50%;
+    border: 1px solid black;
+    background-color: transparent;
+    color: black;
+    font-size: 15px;
+    font-weight: bold;
+  `;
+
+  /* A way to disappear the rectangle when the users needs to highlight
+  where the info is
   const [isHidden, setIsHidden] = useState(false);
 
   const handleHideButtonClick = () => {
@@ -33,8 +49,7 @@ const Rectangle = ({
   if (isHidden) {
     return null;
   }
-
-  //codigo
+*/
 
   const handleHelpButtonHover = () => {
     setShowDescription(true);
@@ -46,19 +61,32 @@ const Rectangle = ({
 
   return (
     <div className="rectangle-with-buttons" style={rectangleStyle}>
-      <div className="example-text">Example</div>
-      <div
-        className="help-button"
-        onMouseEnter={handleHelpButtonHover}
-        onMouseLeave={handleHelpButtonLeave}
-      >
-        ?
-      </div>
-      {showDescription && (
-        <div className="description-box">
-          <p>This is a brief description about the component.</p>
+      <div className="line1" style={{ display: "flex", flexWrap: "nowrap" }}>
+        <div className="target-name" style={{ margin: "0 10px" }}>
+          {currentWord}
         </div>
-      )}
+        <div
+          className="help-button"
+          onMouseEnter={handleHelpButtonHover}
+          onMouseLeave={handleHelpButtonLeave}
+        >
+          <CircleButton>?</CircleButton>
+        </div>
+
+        {showDescription && (
+          <div className="description-box">
+            <p>This is a brief description about the component.</p>
+          </div>
+        )}
+      </div>
+
+      <style>
+        {`.checkbox-container {
+                margin-top:20px;
+                margin-bottom:20px;
+
+            }`}
+      </style>
       <div className="checkbox-container">
         <input
           type="checkbox"
@@ -66,9 +94,16 @@ const Rectangle = ({
           checked={isChecked}
         />
         <label>Validate or </label>
-        <button onClick={handleHideButtonClick}>Select</button>
+        <button /*onClick={handleHideButtonClick}*/>Select</button>
       </div>
 
+      <style>
+        {`.bottom-buttons-container {
+                 display: flex;
+                 justify-content: space-between;           
+                 margin-top:10px;                
+            }`}
+      </style>
       <div className="bottom-buttons-container">
         <button onClick={onReturnClick}>Return</button>
         <button onClick={onNextClick}>Next</button>
