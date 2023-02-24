0909f257c75e2817d1c8e28d980dee6765264da8
Daniel Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2023-02-24 01:12:12 -0400

    Update highlightTool.js

--- src/highlightTool.js
+++ src/highlightTool.js
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from "react";
-import image from "./Slip1.jpg";
+import image1 from "./Images/Slip1.jpg";
+import image2 from "./Images/Slip 1-2.jpg";
+import image3 from "./Images/Slip 1-3.jpg";
+import image4 from "./Images/Slip 1-4.jpg";
+import image5 from "./Images/Slip 1-5.jpg";
+import image6 from "./Images/Slip 1-6.jpg";
+import image7 from "./Images/Slip 1-7.jpg";
+import image8 from "./Images/Slip 1-8.jpg";
+import image9 from "./Images/Slip 1-9.jpg";
+import image10 from "./Images/Slip 1-10.jpg";
+import image11 from "./Images/Slip 1-11.jpg";
+import image12 from "./Images/Slip 1-12.jpg";
 
 function App() {
   const [rect, setRect] = useState({ x: 0, y: 0, width: 0, height: 0 });
@@ -9,7 +20,31 @@ function App() {
     false
   );
   const [blocks, setBlocks] = useState([]);
-  const [filteredText, setFilteredText] = useState([]);
+
+  const images = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12
+  ];
+
+  const [imageIndex, setimageIndex] = useState(0);
+
+  const handlePrev = () => {
+    setimageIndex(imageIndex - 1);
+  };
+
+  const handleNext = () => {
+    setimageIndex(imageIndex + 1);
+  };
 
   useEffect(() => {
     fetch("./pg1_mock_data.json")
@@ -35,7 +70,6 @@ function App() {
         excerpt.push(block.Id);
       }
     });
-    setFilteredText(filtered);
     console.log(`Text: ${filtered.join(" ")}\n\n Excercpt: ${excerpt}`);
   };
 
@@ -103,7 +137,7 @@ function App() {
       onMouseUp={handleMouseUp}
       style={{
         position: "relative",
-        backgroundImage: `url(${image})`,
+        backgroundImage: `url(${images[imageIndex]})`,
         backgroundSize: "cover",
         width: 600,
         height: 800,
@@ -140,6 +174,27 @@ function App() {
           Confirm
         </button>
       )}
+      <button
+        style={{ position: "absolute", top: "730px", left: "10px" }}
+        onClick={() => {
+          handlePrev(true);
+          setShowConfirm(false);
+        }}
+        disabled={imageIndex === 0}
+      >
+        Previous Page
+      </button>
+
+      <button
+        style={{ position: "absolute", top: "730px", left: "450px" }}
+        onClick={() => {
+          handleNext(true);
+          setShowConfirm(false);
+        }}
+        disabled={imageIndex === images.length - 1}
+      >
+        Next Page
+      </button>
     </div>
   );
 }
